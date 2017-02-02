1b61bccf9c0b7ba40e54aff85255080a6af04354
flynn <niceilm@naver.com>
2017-02-02 13:07:32 +0900

    change meteor package

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,29 @@
 import { Observable } from 'rxjs';
-export declare class MeteorPubSub<T> {
+declare module 'meteor/flynn:meteor-helper' {
+  class MeteorPubSub<T> {
     name: string;
+
     constructor(name: string);
+
     register(func: Function): void;
+
     registerComposite(config: PublishCompositeConfig<any> | PublishCompositeConfig<any>[] | ((...args: any[]) => PublishCompositeConfig<any> | PublishCompositeConfig<any>[])): void;
+
     subscribeObservable(...params: any[]): Observable<T>;
-}
-export declare class MeteorMethod<T> {
+  }
+  class MeteorMethod<T> {
     name: string;
     private method;
+
     constructor(name: string);
+
     callObservable(params?: any): Observable<T>;
+
     register(methodOption: ValidatedMethodOption): void;
+
     call(methodArgs: any, callback?: ValidatedMethodCallback): any;
+
     _execute(context: any, methodArgs?: any): any;
+  }
+
 }
